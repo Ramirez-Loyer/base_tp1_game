6f41a965861d3b8d9503844ac4d51c8dfe1c457c
Alejandra Ramirez-Loyer <alejandra.ramirez-loyer@fms-ea.com>
2024-01-25 19:15:25 +0100

    Ajout correction

--- src/game.java
+++ src/game.java
@@ -1,9 +1,59 @@
+/*TP 1 : [BaseTP1Game] Ecrire un programme demandant à  l'utilisateur s'il souhaite jouer à  notre jeu, si non alors sortir du programme, 
+si oui, alors proposer de saisir un chiffre entre 1 et 100 et dire à chaque itération si le chiffre est plus petit ou plus grand. 
+Une fois le chiffre trouvé, afficher un message : vous avez trouvé en x coups !
+Attention, vous devez réaliser le programme en un minimum de lignes de codes tout en veillant à ce qu'il reste parfaitement lisible, 
+enfin que ce soit intuitif pour un autre dev.
+- Puis rendre le jeu permanent tant que le joueur veut jouer, il doit pouvoir continuer
+NB : la méthode Math.random() permet de renvoyer une valeur comprise entre 0 et 0,9*/ 
+
+import java.util.Scanner;
 
 public class game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-	}
+		//boolean = false;
 
-}
+		//Demander à l'utilisateur s'il veut jouer
+
+
+		System.out.println("Bonjour, tu veux jouer ? (Oui/Non) ");
+		Scanner scanner = new Scanner(System.in); 
+		if(scanner.next().equalsIgnoreCase("oui")) {
+
+			//L'utilisateur veut continuer alors ici on code le jeu
+
+			int nombreSaisi =0;
+			int nbrRandom = (int) (Math.random()*100 +1); //On doit caster car la méthode Math.random() pour convertir les nombres entiers -> nombrs réélles
+			int counter = 0;
+
+			System.out.println("Ecris un nombre entre 1 et 100");
+			Scanner scanner2 = new Scanner(System.in);
+
+
+			//Le programme demande de trouver un nombre random
+			while (nombreSaisi != nbrRandom ) {
+				nombreSaisi = scanner2.nextInt();
+				if (nbrRandom > nombreSaisi) {
+					System.out.println("Ecris un nombre plus grand");
+
+				} else if (nbrRandom < nombreSaisi) {
+					System.out.println("Ecris un nombre plus petit");
+
+				} else {
+
+					System.out.println("Bravo! tu as réussi à trouver le nombre en " +  counter +" coups ! ");
+
+				} 
+
+				counter = counter+1; 
+			} //Fin de while
+
+
+		} //Fin if(scanner.next().equalsIgnoreCase("oui"))  
+
+	} // Fin main
+} //fin class
+
+
+
--- src/saisirUneChiffre.java
+++ src/saisirUneChiffre.java
@@ -1,7 +1,7 @@
-/*TP 1 : [BaseTP1Game] Écrire un programme demandant à l'utilisateur s'il souhaite jouer à notre jeu, si non alors sortir du programme, si oui, alors proposer de saisir un chiffre entre 1 et 100 et dire à chaque itération si le chiffre est plus petit ou plus grand. Une fois le chiffre trouvé, afficher un message : vous avez trouvé en x coups !
-Attention, vous devez réaliser le programme en un minimum de lignes de codes tout en veillant à ce qu’il reste parfaitement lisible, enfin que ce soit intuitif pour un autre dev.
+/*TP 1 : [BaseTP1Game] Ecrire un programme demandant à l'utilisateur s'il souhaite jouer à notre jeu, si non alors sortir du programme, si oui, alors proposer de saisir un chiffre entre 1 et 100 et dire à chaque itération si le chiffre est plus petit ou plus grand. Une fois le chiffre trouvé, afficher un message : vous avez trouvé en x coups !
+Attention, vous devez r�aliser le programme en un minimum de lignes de codes tout en veillant à ce qu’il reste parfaitement lisible, enfin que ce soit intuitif pour un autre dev.
 - Puis rendre le jeu permanent tant que le joueur veut jouer, il doit pouvoir continuer
-NB : la méthode Math.random() permet de renvoyer une valeur comprise entre 0 et 0,9*/
+NB : la m�thode Math.random() permet de renvoyer une valeur comprise entre 0 et 0,9*/
 
 
 import java.util.Scanner;
@@ -16,14 +16,14 @@ public class saisirUneChiffre {
 		
 			System.out.println("Bonjour, tu veux jouer ? O/N)");                	// 1. DEMANDER A L'UTILISATEUR S'IL VEUT JOUER
 			
-			String yesno = YesNo.next();	// Initialisation méthode Scanner
+			String yesno = YesNo.next();	// Initialisation m�thode Scanner
 		
 			if(yesno.equalsIgnoreCase("O")){										// Instruction if...else  /  O => true
-																					// La méthode equalsIgnoreCase compare deux strings en ignorant les majuscules/miniscules
+																					// La m�thode equalsIgnoreCase compare deux strings en ignorant les majuscules/miniscules
 				System.out.println("Ecris un nombre entre 1 et 100");		    	// 2. DEMANDER A L'UTILISATEUR D'ECRIRE UN NOMBRE ENTRE 1 ET 100
 					
-					int nbrRandom = (int) (Math.random()*100 +1);					//La méthode Math.random() permet de renvoyer une valeur comprise entre 0 et 0,9, il faut donc multiplier * 100 (0-99) + 1 (1-100)
-					int counter = 1, nbrEntered =0;									// 2 variables : counter => fois joués, nbrEntered => nombre saisi
+					int nbrRandom = (int) (Math.random()*100 +1);					//La m�thode Math.random() permet de renvoyer une valeur comprise entre 0 et 0,9, il faut donc multiplier * 100 (0-99) + 1 (1-100)
+					int counter = 1, nbrEntered =0;									// 2 variables : counter => fois jou�s, nbrEntered => nombre saisi
 					
 					while (nbrEntered != nbrRandom) {								// BOUCLE : Tant que le nombre écrit est différent du nombre généré aléatoirement
 						nbrEntered = YesNo.nextInt();								// l'utilisateur doit saisir un autre 
@@ -31,7 +31,9 @@ public class saisirUneChiffre {
 							System.out.println("Ecris un nombre plus petit");   	// Si ce nouveau nombre est inférieure à celui généré aléatoirement alors l'utilisateur soit écrire une valeur plus petite
 						else if(nbrEntered < nbrRandom)
 							System.out.println("Ecris un nombre plus grand ");  	// Si ce nouveau nombre est supérieur à celui généré aléatoirement alors l'utilisateur soit écrire une valeur plus grande
-						else System.out.println("Tu as trouvé en " + counter + " coups"); counter++;
+						else System.out.println("Tu as trouvé en " + counter + " coups"); 
+						counter++;
+					
 					}																// Si l'utilisateur trouve le nombre le jeu s'arrête et un phrase lui indique le nombre de coups joués / Concatenation -counter
 					
 					System.out.println(nbrRandom);
